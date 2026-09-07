2533f2d29b8abc945a612cac6aa9666d12d0c146
imran <immui1413@gmail.com>
2026-09-08 00:32:02 +0530

    complted data-types

---
+++ python/data-types.py
@@ -0,0 +1,37 @@
+# basic data types(int,float,complex,boolean, string)
+# container data types(list,tuple,sets,dictionary)
+# user defined data types = class
+
+# intiger
+print(5)
+print(1e309)
+
+#float
+print(5.5)
+print(1.7e309)
+
+# boolean
+print(True)
+print(False)
+
+# complex
+print(3+5j)
+
+# strings
+
+print('imran')
+print("imran")
+print("""imran""")
+
+# lists
+print([1,2,3,4,5])
+
+
+# tuples
+print((1,2,3,4,5))
+
+#sets
+print({1,2,3,4,5})
+
+#dictionary
+print({"name":"imran","age":20,"gender":"male"})
