a9a94cb60685e3478d4ac1b44a1c495c1a2ae277
imran <immui1413@gmail.com>
2026-09-08 00:53:44 +0530

    day 1 of learning python complted see u tmr :)

---
+++ python/variables.py
@@ -0,0 +1,36 @@
+# unlike c lang python doesn't need variable declairation like 
+# int a = 5;
+#example in python we just a give a name and value
+name = 'imran'
+print(name)
+#it automatically ditects data-type
+#this feature called dynamic typing
+
+name = 4
+print(name)
+
+name = True
+print(name)
+
+#dynamic binding
+# a varible that can store multiple data-types
+
+#sspecial syntac
+
+a=1;b=2;c=3
+print(a)
+print(b)
+print(c)
+
+a,b,c = 1,2,3
+
+print(a)
+print(b)
+print(c)
+
+a=b=c=6
+
+print(a)
+print(b)
+print(c)
+
