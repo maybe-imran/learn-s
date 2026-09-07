7b70fb12ae7e2b96f9e15b2b41febd1792114e3a
imran <immui1413@gmail.com>
2026-09-08 00:15:59 +0530

    commit print.py

---
+++ python/print.py
@@ -0,0 +1,16 @@
+print("hellow")
+
+print(10)
+
+print(True)
+
+print(10.5)
+
+print("hellow",end=" ")
+print("world")
+
+print("india","japan","china",sep="/")
+
+print("india","japan","china",sep="-")
+
+print("india","japan","china",sep=" ")
