260fcc72029beb8ed41517811045fdb53c861ded
imran <immui1413@gmail.com>
2026-09-09 23:26:28 +0530

    complted keywrods and identifiers

---
+++ python/keywords-identifiers.py
@@ -0,0 +1,19 @@
+# keywords r special type of words that r given by the lang
+#python has 33 keywords
+import keyword
+# importing keywords lib
+print(keyword.kwlist)
+#printing all key-words 
+    # IDENTIFIERS
+        # is a name used to verify the variballe,function,class, modules or the objects 
+        # rules 
+            # cannot start with number 
+            #and soo on 
+
+
+
+name = "imran" 
+print(name)
+
+_ = "chudail"
+print(_)
--- python/keywords.py
+++
@@ -1,6 +0,0 @@
-# keywords r special type of words that r given by the lang
-#python has 33 keywords
-import keyword
-# importing keywords lib
-print(keyword.kwlist)
-#printing all key-words 
